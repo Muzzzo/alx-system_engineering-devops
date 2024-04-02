d852a07740bf3b0f00fc3e2d6b355a8c08f341fe
Muzzo <51519604+Muzzzo@users.noreply.github.com>
2024-04-02 22:40:31 +0000

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# puppet fix for code 500
-
-exec { 'fix-wordpress':
-  command  => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  provider => shell,
-  path     => '/usr/local/sbin:/usr/local/bin:usr/sbin:/usr/bin:/sbin:/bin',
-}
